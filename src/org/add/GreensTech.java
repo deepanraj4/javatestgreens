aeb9efdc9d2beb5fc45d3736ee4c5f79cb9e82bf
deepanraj4 <deepan4x@gmail.com>
2023-10-04 20:10:36 +0530

    java test address

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,25 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensVelachery() {
+		System.out.println ("velachery Adress");	
+	}
+	private void greensAdayar() {
+		System.out.println ("velachery Adyar");	
+	}
+	private void greensTambaram() {
+		System.out.println ("velachery Adress");	
+	}
+	private void greensAnnanagar() {
+		System.out.println ("velachery Adress");	
+	}
+public static void main (String[] args) {
+	
+	GreensTech g=new GreensTech ();
+	g.greensVelachery();
+	g.greensAdayar();
+	g.greensTambaram();
+	g.greensAnnanagar();
+}
+}
